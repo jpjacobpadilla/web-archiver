c272b808564e0e8185732a9efd8c2ae893c6d42d
Jacob Padilla <jp@jacobpadilla.com>
2025-08-28 22:47:44 -0400

    group by scraping job id

--- backend/app/sql/get_archived_sites.sql
+++ backend/app/sql/get_archived_sites.sql
@@ -5,5 +5,5 @@ select
     count(distinct aj.id) as job_count
 from archived_resource ar
 join archive_jobs aj on ar.scraping_job = aj.id
-group by ar.host
+group by ar.scraping_job
 order by latest_job_time desc;
